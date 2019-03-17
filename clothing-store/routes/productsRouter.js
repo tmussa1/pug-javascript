29d43dbf2998ac42c38d47f27e704adb12117504
Tofik Mussa <tofahub@gmail.com>
2019-03-16 23:12:09 -0400

    Jade is not iterating

---
+++ clothing-store/routes/productsRouter.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var productsDB = require('.././model/productsDB');
+
+router.get('/', (req, res, next) =>{
+    var listOfProducts = [];
+    listOfProducts = productsDB.products;
+    res.render('products', listOfProducts);
+    next();
+});
+
+router.post('/addProduct', (req, res, next)=>{
+    var newProduct = {
+        productId: req.body.productId,
+        productName: req.body.productName,
+        price: req.body.price,
+        description: req.body.description
+    };
+    productsDB.products.push(newProduct);
+    res.redirect('/products/');
+});
+
+module.exports = router;

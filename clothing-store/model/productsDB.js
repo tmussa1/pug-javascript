2c5330c6d2305dbaa085e75874a2275132d881f2
Tofik Mussa <tofahub@gmail.com>
2019-03-30 20:20:24 -0400

    setup again

--- clothing-store/model/productsDB.js
+++ clothing-store/model/productsDB.js
@@ -2,18 +2,21 @@ module.exports.products = [{
         productId: "1",
         productName: "kaftan",
         price : 60,
-        description: "Women's dress"
+        description: "Women's dress",
+        imageUrl: ""
         },
         {
         productId: "2",
         productName: "Shorts",
         price: 40,
-        description: "Summer is coming"
+        description: "Summer is coming",
+        imageUrl: ""
         }, 
         {
         productId: "3",
         productName: "Tshirt",
         price: 10,
-        description: "Suns out guns out"
+        description: "Suns out guns out",
+        imageUrl: ""
         }
     ];
--- clothing-store/routes/productsRouter.js
+++ clothing-store/routes/productsRouter.js
@@ -1,20 +1,21 @@
+var multer = require('multer');
+var upload = multer({dest : '/public/images'})
 var express = require('express');
 var router = express.Router();
 var productsDB = require('.././model/productsDB');
 
 router.get('/', (req, res, next) =>{
-    //var listOfProducts = productsDB.products;
     res.render('products', {listOfProducts : productsDB.products});
-    //res.send(listOfProducts);
     next();
 });
 
-router.post('/addProduct', (req, res, next)=>{
+router.post('/addProduct', upload.single('imageUrl'), (req, res, next)=>{
     var newProduct = {
         productId: req.body.productId,
         productName: req.body.productName,
         price: req.body.price,
-        description: req.body.description
+        description: req.body.description,
+        imageUrl: req.file.filename
     };
     productsDB.products.push(newProduct);
     res.redirect('/products/');
